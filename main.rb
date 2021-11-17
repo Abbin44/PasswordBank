cf923401e8d7b0d7327b15d1e7583a5182ece3cb
Abbin44 <abbin44@gmail.com>
2021-11-17 22:56:29 +0100

    Modified program to accept flags instead of an interactive approach

--- main.rb
+++ main.rb
@@ -12,8 +12,12 @@ class Main
     end
 
     def decrypt_passwd passwd
-        dec = AES.decrypt(passwd, $master_key)
-        return dec
+        begin
+            dec = AES.decrypt(passwd, $master_key)
+            return dec
+        rescue => error
+            return passwd #If decryption fails, return the encrypted password to ensure security
+        end
     end
 
     def view_all_passwords
@@ -25,12 +29,7 @@ class Main
         end
     end
 
-    def generate_new
-        system ("clear") || (system "cls")
-        puts "Enter the website you want to create a new password for: "
-        website = gets.chomp()
-        puts "Enter the password you want: "
-        password = gets.chomp()
+    def generate_new website, password
         password = enc_passwd password
 
         if not File.exist?($save_file)
@@ -39,29 +38,35 @@ class Main
 
         File.open($save_file, "a") {|f| f.puts website + ":" + password}
     end
+end
 
-    def start
-        puts "Enter master key"
-        $master_key = gets.chomp()
-        system ("clear") || (system "cls")
-
-        puts "1) View stored passwords"
-        puts "2) Generate a new password"
-        puts "3) Help/Information"
-
-        input = gets.chomp()
-        case input
-            when "1"
-                view_all_passwords
-            when "2"
-                generate_new
-            when "3"
-
-            else
-                puts "invalid input"
+class ArgReader
+    $flags = ARGV
+    puts $flags.join(" - ")
+    def process_argv
+        main_obj = Main.new
+        for arg in 0 ... $flags.length() do
+            case $flags[arg]
+                when "-k"
+                    $master_key = $flags[arg + 1]
+                when "-v"
+                    main_obj.view_all_passwords
+                when "-n"
+                    credentials = $flags[arg + 1]
+                    split = credentials.split(":")
+                    website = split[0]
+                    password = split[1]
+                    main_obj.generate_new website, password
+                when "-h"
+                    puts "-k provides the key for the encryption and decryption"
+                    puts "-v shows all stored passwords "
+                    puts "-n creates a new account. The format is '-n website:password'"
+            end
         end
     end
 end
 
-object = Main.new
-object.start
+args_obj = ArgReader.new
+args_obj.process_argv
+#main_obj = Main.new
+#main_obj.start
