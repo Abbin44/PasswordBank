d9794254eab92514ad077185205196af1284f0da
Abbin44 <abbin44@gmail.com>
2021-11-21 22:27:03 +0100

    Added remove and search flags 	modified:   main.rb

--- main.rb
+++ main.rb
@@ -4,7 +4,36 @@ require "test/unit/assertions"
 include Test::Unit::Assertions
 
 class Main
-    $save_file = "/passwords.key"
+    $save_file = "./passwords.key"
+
+    def remove_account website
+	    lines = File.readlines($save_file)
+	    File.open($save_file, 'w'){|file| file.truncate(0)} #Empty the files content
+
+	    for i in 0 ... lines.length()
+	    	next if lines[i] == nil
+	    	if lines[i].start_with?(website)
+	    		lines.delete_at(i)
+	    	end
+	    end
+
+	    	File.open($save_file, 'w+') do |f|
+	    		f.puts lines
+	    end
+    end
+
+    def search_for_website website
+        lines = File.readlines($save_file)
+
+        for i in 0 ... lines.length()
+            next if lines[i] == nil
+            if lines[i].start_with?(website)
+                tokens = lines[i].split(":")
+                pass = decrypt_passwd tokens[1]
+                puts(tokens[0] + " : " + pass)
+            end
+        end
+    end
 
     def enc_passwd passwd
         enc = AES.encrypt(passwd, $master_key)
@@ -42,15 +71,19 @@ end
 
 class ArgReader
     $flags = ARGV
-    puts $flags.join(" - ")
+
     def process_argv
         main_obj = Main.new
         for arg in 0 ... $flags.length() do
             case $flags[arg]
                 when "-k"
                     $master_key = $flags[arg + 1]
-                when "-v"
+                when "-l"
                     main_obj.view_all_passwords
+                when "-s"
+                    main_obj.search_for_website $flags[arg + 1]
+                when "-r"
+                    main_obj.remove_account $flags[arg + 1]
                 when "-n"
                     credentials = $flags[arg + 1]
                     split = credentials.split(":")
@@ -59,8 +92,10 @@ class ArgReader
                     main_obj.generate_new website, password
                 when "-h"
                     puts "-k provides the key for the encryption and decryption"
-                    puts "-v shows all stored passwords "
+                    puts "-l shows all stored passwords "
+                    puts "-s searches and prints the provided website/username"
                     puts "-n creates a new account. The format is '-n website:password'"
+		    puts "-r to remove credentials of inserted username/website"
             end
         end
     end
@@ -68,5 +103,3 @@ end
 
 args_obj = ArgReader.new
 args_obj.process_argv
-#main_obj = Main.new
-#main_obj.start
